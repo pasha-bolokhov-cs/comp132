711c6eba4eec230118e823100309aa9eea7e5c2f
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-17 11:00:40 -0800

    Edit more

--- workplace/Assign4/src/assign4/Register.java
+++ workplace/Assign4/src/assign4/Register.java
@@ -8,23 +8,25 @@ public class Register {
 	// instance variables to help you get started. 
 	
 	private int _100, _50, _20, _10, _5, _2, _1, quarter;
-	private double remaining, amountReceived, changeDue;
+	private double remaining, received, changeDue;
 	
 	// default constructor
 	public Register() {
-	
+		_100 = _50 = _20 = _10 = _5 = _2 = _1 = quarter = 0;
+		remaining = amountReceived = changeDue = 0.0;
 	}
 	
 	// a method to return the change due and calculate the 
 	// values within each denomination (only up to quarters. 
 	public double change(double amountDue, double amountReceived) {
-	    /***** TO DO *****/
-	    return changeDue;
+		received = amountReceived;
+		
+	    	return changeDue;
 	}
 	
 	// string representation of the object. 
 	public String toString() {
-	    /***** TO DO *****/
-	    return null;   
+		/***** TO DO *****/
+		return null;   
 	}
 }
