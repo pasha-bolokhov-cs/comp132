66605f4ef322591d4aa0b610d79a08b8a0cb10ec
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-17 10:57:00 -0800

    Edit more

---
+++ workplace/Assign4/src/assign4/Assign4.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assign4;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+/**
+ *
+ * @author saryta
+ */
+public class Assign4 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		String itemName;
+		int quantity;
+		double price;
+		Scanner scan = new Scanner(System.in);
+		
+		Cart cart = new Cart();
+		
+		System.out.println("Welcome to Shopper's Paradise");
+		System.out.println();
+		
+		System.out.print("Enter the name of the first item: ");
+		itemName = scan.nextLine();
+		while (!itemName.equals("q") && !itemName.equals("Q")) {
+			System.out.print ("Enter the quantity: ");
+			quantity = scan.nextInt();
+			System.out.print ("Enter the price: ");
+			price = scan.nextDouble();
+			scan.nextLine();    // read over to the end of line
+		
+			cart.addToCart(itemName, price, quantity);
+		
+			System.out.print ("Enter the name of the next item or Q to quit: ");
+			itemName = scan.nextLine();
+		}
+		
+		System.out.println();
+		System.out.println(cart);
+		System.out.println();
+		
+		NumberFormat fmt = NumberFormat.getCurrencyInstance();
+		System.out.println ("Please pay... " + fmt.format(cart.getTotal()));
+		
+//		System.out.print("How much are you giving me? ");
+//		double amountReceived = scan.nextDouble();
+//		Register r = new Register();
+//		System.out.println("Your change is: " + fmt.format(r.change(cart.getTotal(), amountReceived)));
+//		System.out.println(r.toString());
+	}
+}
--- workplace/Assign4/src/assign4/Register.java
+++ workplace/Assign4/src/assign4/Register.java
@@ -1,31 +1,30 @@
-package lab4;
+package assign4;
 
 
 /* 
   Reuse your logic from Lab 2
 */
 public class Register {
-
-    // instance variables to help you get started. 
-    
-    private int _100, _50, _20, _10, _5, _2, _1, quarter;
-    private double remaining, amountReceived, changeDue;
-
-    // default constructor
-    public Register() {
-   
-    }
-
-    // a method to return the change due and calculate the 
-    // values within each denomination (only up to quarters. 
-    public double change(double amountDue, double amountReceived) {
-        /***** TO DO *****/
-        return changeDue;
-    }
-
-    // string representation of the object. 
-    public String toString() {
-        /***** TO DO *****/
-        return null;   
-    }
+	// instance variables to help you get started. 
+	
+	private int _100, _50, _20, _10, _5, _2, _1, quarter;
+	private double remaining, amountReceived, changeDue;
+	
+	// default constructor
+	public Register() {
+	
+	}
+	
+	// a method to return the change due and calculate the 
+	// values within each denomination (only up to quarters. 
+	public double change(double amountDue, double amountReceived) {
+	    /***** TO DO *****/
+	    return changeDue;
+	}
+	
+	// string representation of the object. 
+	public String toString() {
+	    /***** TO DO *****/
+	    return null;   
+	}
 }
