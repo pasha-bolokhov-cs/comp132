98aafd9e5fb003ff6681da4a8835111053af7c1e
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-26 10:16:57 -0800

    Edit more

--- assign5/assign5/assign5.java
+++ assign5/assign5/assign5.java
@@ -6,16 +6,16 @@ import media.*;
 
 public class Assign5 {
 
-    public static void main(String[] args) {
-
-        Picture pic = new Picture (FileChooser.pickAFile());
-        pic.show();
-
-        pic.increaseRed();
-        pic.changeRed(.5);
-        pic.decreaseRed();
-        pic.repaint();
-
-
-    }
+	public static void main(String[] args) {
+	
+		Picture pic = new Picture (FileChooser.pickAFile());
+		pic.show();
+		
+		pic.increaseRed();
+		pic.changeRed(.5);
+		pic.decreaseRed();
+		pic.repaint();
+	
+	
+	}
 }
