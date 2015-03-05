ab406a47c3d2e1aaeab3937e0a50221ccfa17504
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-03-05 10:52:27 -0800

    Edit more

--- assign6/assign6/Assign6.java
+++ assign6/assign6/Assign6.java
@@ -11,14 +11,19 @@ public class Assign6 {
 		Picture pic = new Picture ("assign6/night.jpg");
 		Picture spare = new Picture(pic);			// save the picture
 		pic.show();
-		pic.runEffect();
+		Thread.sleep(800);
+		pic.runEffect();					// run the effect
 		pic.repaint();
+		Thread.sleep(800);
 
+		double angle = 0.0;
 		while (true) {
-			Thread.sleep(800);
-			Picture other = new Picture(spare);
-			other.rotateThisImage(30.0);
-			other.repaint();
+			Thread.sleep(1200);
+			pic.hide();
+			pic = null;					// delete the picture
+			pic = new Picture(spare);			// take the original picture
+			pic.rotateThisImage(angle += 30.0);
+			pic.repaint();
 		}
 	}
 }
